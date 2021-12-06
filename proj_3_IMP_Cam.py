82247cf8281d338b7f5db9f562ec2ae6f1eee367
Isaiah P <45181513+Theyounggod@users.noreply.github.com>
2021-12-05 21:22:14 -0600

    Add files via upload

---
+++ proj_3_IMP_Cam.py
@@ -0,0 +1,45 @@
+import cv2
+import numpy as np
+import onnxruntime as ort
+
+
+def center_crop(frame):
+    h, w, _ = frame.shape
+    start = abs(h - w) // 2
+    if h > w:
+        return frame[start: start + w]
+    return frame[:, start: start + h]
+
+
+def main():
+
+
+    index_to_letter = list('ABCDEFGHIKLMNOPQRSTUVWXY')
+    mean = 0.485 * 255.
+    std = 0.229 * 255.
+    ort_session = ort.InferenceSession("signlanguage.onnx")
+
+    cap = cv2.VideoCapture(0)
+    while True:
+        ret, frame = cap.read()
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+        x = cv2.resize(frame, (28, 28))
+        x = (x - mean) / std
+
+        x = x.reshape(1, 1, 28, 28).astype(np.float32)
+        y = ort_session.run(None, {'input': x})[0]
+
+        index = np.argmax(y, axis=1)
+        letter = index_to_letter[int(index)]
+
+        cv2.putText(frame, letter, (500, 500), cv2.FONT_HERSHEY_SIMPLEX, 2.0, (255, 0, 255), thickness=2)
+        cv2.imshow("my cam", frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('i'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
